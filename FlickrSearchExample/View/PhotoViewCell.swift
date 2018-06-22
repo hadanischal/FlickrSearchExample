8bc0a9dc3ece402cff4f04bfa3843cc8a139d3e2
Nischal Hada <hadanischal@gmail.com>
2018-06-22 19:38:25 +1000

    added collection view cell

---
+++ FlickrSearchExample/View/PhotoViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoViewCell.swift
+//  FlickrSearchExample
+//
+//  Created by Nischal Hada on 6/22/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewCell: UICollectionViewCell {
+    @IBOutlet weak var bagroundView: UIView!
+    @IBOutlet var titleLabel: UILabel?
+    @IBOutlet var photoImageView: UIImageView?
+
+    var photosValue : PhotosModel? {
+        didSet {
+            guard let feeds = photosValue else {
+                return
+            }
+            titleLabel?.text = feeds.title
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.bagroundView.backgroundColor = ThemeColor.contentViewBackgroundColor
+        self.photoImageView?.contentMode =   UIViewContentMode.scaleAspectFit
+    }
+}
