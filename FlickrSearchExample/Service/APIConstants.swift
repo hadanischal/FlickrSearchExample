a843c9afe2a07f2e8d7d09842deb3d1d731748a7
Nischal Hada <hadanischal@gmail.com>
2018-06-22 16:28:38 +1000

    added APIConstants

---
+++ FlickrSearchExample/Service/APIConstants.swift
@@ -0,0 +1,34 @@
+//
+//  APIConstants.swift
+//  FlickrSearchExample
+//
+//  Created by Nischal Hada on 6/22/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+struct APIConstants {
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+//    private static let photoSearch = "flickr.photos.search"
+//    private static let Key = "63f750f7c42662239caa7482194185b4"
+//    private static let Secret = "fd9869de82b522fa?"
+//
+    
+    static let flickrMethod =  "flickr.photos.search"
+    static let apiKeyValue  =  "739b660ea3629666d04b83ad0a19a381"
+    static let limit        =  20
+    
+    static let methodKey =         "method"
+    static let apiKey    =         "api_key"
+    static let nojsoncallbackKey = "nojsoncallback"
+    static let formatKey =         "format"
+    static let safeSearchKey =     "safe_search"
+    static let textKey =           "text"
+    static let perPageKey =        "per_page"
+    static let pageKey =           "page"
+
+
+    
+ 
+}
