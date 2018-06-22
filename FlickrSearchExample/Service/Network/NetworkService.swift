80b179a877a34de57714c0402d5a4389aaadede4
Nischal Hada <hadanischal@gmail.com>
2018-06-22 16:42:14 +1000

    added class for NetworkHandler and Network request

---
+++ FlickrSearchExample/Service/Network/NetworkService.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkService.swift
+//  FlickrSearchExample
+//
+//  Created by Nischal Hada on 6/22/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+final class NetworkService {
+    func loadData(url: URL, session: URLSession = URLSession(configuration: .default), completion: @escaping (Result<Data, ErrorResult>) -> Void) -> URLSessionTask? {
+        var request = NetworkMethod.request(method: .GET, url: url)
+        if let reachability = Reachability(), !reachability.isReachable {
+            request.cachePolicy = .returnCacheDataDontLoad
+        }
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(.network(string: "An error occured during request :" + error.localizedDescription)))
+                return
+            }
+            if let data = data {
+                completion(.success(data))
+            }
+        }
+        task.resume()
+        return task
+    }
+}
