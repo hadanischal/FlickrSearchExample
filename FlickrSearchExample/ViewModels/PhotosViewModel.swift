40e826b88bdda7e83a83acfa14d6f1876afe378d
Nischal Hada <hadanischal@gmail.com>
2018-06-22 19:53:03 +1000

    added PhotosViewModel handelled methods on it

--- FlickrSearchExample/ViewModels/PhotosViewModel.swift
+++ FlickrSearchExample/ViewModels/PhotosViewModel.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+class  PhotosViewModel {
+    weak var dataSource : GenericDataSource<PhotosModel>?
+    weak var service: PhotosServiceCallProtocol?
+    
+    init(service: PhotosServiceCallProtocol, dataSource : GenericDataSource<PhotosModel>?) {
+        self.dataSource = dataSource
+        self.service = service
+    }
+    
+    func fetchServiceCall(_ completion: ((Result<Bool, ErrorResult>) -> Void)? = nil) {
+        
+        guard let service = service else {
+            completion?(Result.failure(ErrorResult.custom(string: "Missing service")))
+            return
+        }
+        service.fetchPhotos { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let converter) :
+                    self.dataSource?.data.value = converter.photoResults
+                    completion?(Result.success(true))
+                    break
+                case .failure(let error) :
+                    print("Parser error \(error)")
+                    completion?(Result.failure(error))
+                    break
+                }
+            }
+        }
+    }
+}
