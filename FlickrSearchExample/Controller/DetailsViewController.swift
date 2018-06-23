983d681dc2c0299537fad4584652d12286e1db2c
Nischal Hada <hadanischal@gmail.com>
2018-06-23 12:39:45 +1000

    DetailsViewController UI setup

--- FlickrSearchExample/Controller/DetailsViewController.swift
+++ FlickrSearchExample/Controller/DetailsViewController.swift
@@ -9,28 +9,18 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     var viewModel: DetailsViewModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        self.navigationItem.title = viewModel?.dataSource?.title
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
-    */
+
 
 }
