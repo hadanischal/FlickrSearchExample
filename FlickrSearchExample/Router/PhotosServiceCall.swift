eb54d2c8a0c2d0049db1b5301073f0558a68f633
Nischal Hada <hadanischal@gmail.com>
2018-08-01 18:41:19 +1000

    added APIKey constant

--- FlickrSearchExample/Router/PhotosServiceCall.swift
+++ FlickrSearchExample/Router/PhotosServiceCall.swift
@@ -22,23 +22,23 @@ final class PhotosServiceCall: NetworkHandler, PhotosServiceCallProtocol {
         self.cancelFetchService()
 
         var parameters : [String: String] = [
-            APIConstants.methodKey:          APIConstants.APIMethods_PhotosSearch,
-            APIConstants.apiKey:             APIConstants.apiKeyValue,
-            APIConstants.nojsoncallbackKey:  "1",
-            APIConstants.formatKey:          "json",
-            APIConstants.safeSearchKey:      "1",
-            APIConstants.textKey:            searchTerm,
-            APIConstants.perPageKey:         APIConstants.limit,
-            APIConstants.pageKey:            "1" //Default for now
+            APIKey.methodKey: APIConstants.APIMethods_PhotosSearch,
+            APIKey.apiKey: APIConstants.apiKeyValue,
+            APIKey.nojsoncallbackKey: "1",
+            APIKey.formatKey: "json",
+            APIKey.safeSearchKey: "1",
+            APIKey.textKey: searchTerm,
+            APIKey.perPageKey: APIConstants.limit,
+            APIKey.pageKey: "1" //Default for now
         ]
 
         if searchTerm.isEmpty{
             guard let location = LocationService.sharedInstance.lastLocation else {
                 return
             }
-            parameters[APIConstants.latKey] = "\(location.coordinate.latitude)" //APIConstants.latConst
-            parameters[APIConstants.longKey] = "\(location.coordinate.longitude)" // APIConstants.lonConst
-            parameters[APIConstants.textKey] = "Nearby"
+            parameters[APIKey.latKey] = "\(location.coordinate.latitude)"
+            parameters[APIKey.longKey] = "\(location.coordinate.longitude)"
+            parameters[APIKey.textKey] = "Nearby"
         }
 
         task = NetworkService().loadData(urlString: endpoint, parameters: parameters, completion: self.networkResult(completion: completion))
